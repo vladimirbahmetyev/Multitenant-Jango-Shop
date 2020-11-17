5669cc0381d748fb7d95832160b6ca6db4663d38
Vladimir <vbakhmetev@emphasoft.com>
2020-11-17 21:59:38 +0300

    Removed tennant middleware
    Will use "setTenant" in views.py

--- multitenant/multitenant/settings.py
+++ multitenant/multitenant/settings.py
@@ -60,7 +60,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'shop.middleware.MultitenantMiddleware',
 ]
 
 ROOT_URLCONF = 'multitenant.urls'
