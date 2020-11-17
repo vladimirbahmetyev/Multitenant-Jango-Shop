187388025e1d73ebc4147286e8c1319ef0e3087c
Vladimir <vbakhmetev@emphasoft.com>
2020-11-17 21:53:15 +0300

    Revert "Merge branch 'main' of https://github.com/vladimirbahmetyev/Multitenant-Jango-Shop into main"
    This reverts commit 12e3debb0eee2ea62b5169138ef88474561072a8, reversing
    changes made to cd11033ada33cdae551058abb50d73b4ac804d84.

--- multitenant/multitenant/apps/shop/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-
-class Item(models.Model):
-    item_name = models.CharField(max_length=50)
-    price = models.IntegerField()
-    is_available = models.BooleanField()
-    picture = models.FileField()
