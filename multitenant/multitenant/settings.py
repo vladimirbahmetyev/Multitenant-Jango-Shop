0eef734ab35546ba1a64e1e36c5bb27243594ae6
Vladimir <vbakhmetev@emphasoft.com>
2020-12-07 00:36:23 +0300

    fixed3

--- multitenant/multitenant/settings.py
+++ multitenant/multitenant/settings.py
@@ -174,7 +174,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 VENV_PATH = os.path.dirname(BASE_DIR)
